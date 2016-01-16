2d40b78bd1bd6b29918bad237ebacc3e3b82da32
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-01-16 11:20:53 +0100

    Remove Lestcert.run.

--- lib/letscert.rb
+++ lib/letscert.rb
@@ -1,15 +1,9 @@
-require_relative 'letscert/runner'
-
-
+# Namespace for all letcert's classes.
 module LetsCert
 
   # Letscert version number
   VERSION = '0.0.1'
 
-  # run {Runner.run}
-  # @return [void]
-  def self.run
-    Runner.run
-  end
-
 end
+
+require_relative 'letscert/runner'
