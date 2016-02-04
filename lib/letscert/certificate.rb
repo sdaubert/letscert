ca7e592d019119ff0a8a069bedf85c702bbb7982
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-02-04 15:28:56 +0100

    Add new Certificate class to handle ACME operations on certificates.

---
+++ lib/letscert/certificate.rb
@@ -0,0 +1,15 @@
+
+module LetsCert
+
+  # Class to handle ACME operations on certificates
+  class Certificate
+
+    def self.revoke(files)
+      @logger.warn "revoke not yet implemented"
+    end
+
+    def self.get(data)
+    end
+  end
+
+end
