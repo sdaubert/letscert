a67bcfd6fff4059c17a9f5c9b5b2370a1c163404
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-02-16 14:13:44 +0100

    Add void specs for Certificate#valid?.

---
+++ spec/certificate_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'spec_helper'
+
+module LetsCert
+
+  describe Certificate do
+
+    context '#valid?' do
+      it 'checks whether a certificate is valid given a minimum valid duration'
+      it 'checks whether a certificate validates all given domains'
+    end
+
+  end
+
+end
