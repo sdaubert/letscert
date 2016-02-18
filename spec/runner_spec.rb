04bc8580b5bd75de3d80d295642343fcefbda26e
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-02-18 12:32:55 +0100

    Write spec for --default-root option.

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -41,14 +41,36 @@ module LetsCert
         expect(runner.options[:domains]).to include('www2.example.com')
       end
 
-      it 'sets default root path with --default-root for domains without PATH'
-      it 'accepts multiples files with --file option'
-      it '--file option only accepts some predefined values'
+      it 'sets default root path with --default-root for domains without PATH' do
+        ARGV << '--domain' << 'example.com'
+        ARGV << '--domain' << 'another-example.com:/var/www/html'
+        ARGV << '--default-root' << '/opt/www'
+
+        runner.parse_options
+        expect(runner.options[:default_root]).to eq('/opt/www')
+        expect(runner.options[:roots]).to be_a(Hash)
+        expect(runner.options[:roots]['example.com']).to eq(runner.options[:default_root])
+        expect(runner.options[:roots]['another-example.com']).to eq('/var/www/html')
+      end
+
+      it 'accepts multiples files with --file option' do
+        ARGV << '--file' << 'key.pem'
+        ARGV << '-f' << 'cert.pem'
+        
+        runner.parse_options
+        expect(runner.options[:files]).to be_a(Array)
+        expect(runner.options[:files].size).to eq(2)
+        expect(runner.options[:files]).to include('key.pem')
+        expect(runner.options[:files]).to include('cert.pem')
+      end
+
       it 'sets minimum validity time with --valid-min option'
       it '--valid-min option accepts minute format'
       it '--valid-min option accepts hour format'
       it '--valid-min option accepts day format'
     end
+
+    it '#check_persisted checks all mandatory components are covered by files'
 
   end
 
