498ff76cdb8f740a24b1ecd272a36e7f121aebd6
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-02-18 12:32:55 +0100

    Write spec for --default-root option.

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -41,7 +41,18 @@ module LetsCert
         expect(runner.options[:domains]).to include('www2.example.com')
       end
 
-      it 'sets default root path with --default-root for domains without PATH'
+      it 'sets default root path with --default-root for domains without PATH' do
+        ARGV << '--domain' << 'example.com'
+        ARGV << '--domain' << 'another-example.com:/var/www/html'
+        ARGV << '--default-root' << '/opt/www'
+
+        runner.parse_options
+        expect(runner.options[:default_root]).to eq('/opt/www')
+        expect(runner.options[:roots]).to be_a(Hash)
+        expect(runner.options[:roots]['example.com']).to eq(runner.options[:default_root])
+        expect(runner.options[:roots]['another-example.com']).to eq('/var/www/html')
+      end
+
       it 'accepts multiples files with --file option'
       it '--file option only accepts some predefined values'
       it 'sets minimum validity time with --valid-min option'
