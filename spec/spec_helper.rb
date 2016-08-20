9e801c0e9efd6342cf175af0b4d82d772d965aba
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-08-20 19:20:45 +0200

    Add cassettes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ require 'fileutils'
 
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/cassettes"
   config.hook_into :faraday
 end
 
