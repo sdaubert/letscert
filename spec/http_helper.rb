8fb7da85dcbea33566c733f8b43806aa35384358
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-08-22 20:37:48 +0200

    Remove duplicate code.

--- spec/http_helper.rb
+++ spec/http_helper.rb
@@ -3,13 +3,11 @@ module HttpHelper
     return yield unless VCR.real_http_connections_allowed?
 
     null_logger = Logger.new(StringIO.new)
-    webrick = webrick = WEBrick::HTTPServer.new(Port: 5002, DocumentRoot: path,
-                                                Logger: null_logger,
-                                                AccessLog: null_logger)
+    webrick = WEBrick::HTTPServer.new(Port: 5002, DocumentRoot: path,
+                                      Logger: null_logger, AccessLog: null_logger)
 
     begin
       thread = Thread.new { webrick.start }
-
       yield
     ensure
       webrick.shutdown
