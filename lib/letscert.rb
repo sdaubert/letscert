e5a1362a45a47b64d9261a5d48307a0782e33909
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-08-26 18:08:38 +0200

    Extract VERSION from letscert.rb and put it into letscert/version.rb.

--- lib/letscert.rb
+++ lib/letscert.rb
@@ -23,13 +23,10 @@
 # Namespace for all letcert's classes.
 module LetsCert
 
-  # Letscert version number
-  VERSION = '0.4.0'
-
-
   # Base error class
   class Error < StandardError; end
 
 end
 
+require_relative 'letscert/version'
 require_relative 'letscert/runner'
