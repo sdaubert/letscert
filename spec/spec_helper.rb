3dc75f8c24c25305ef09373b64fb7d4eb80e18d5
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-10-18 08:18:53 +0200

    Extract certificate helper methods from spec_helper.rb to CertificateHelper module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |c|
   c.include HttpHelper
   c.include FileHelper
+  c.include CertificateHelper
 end
 
 VCR.configure do |config|
@@ -63,40 +64,4 @@ def add_option(option, value=nil)
   dash = option.size == 1 ? '-' : '--'
   ARGV << "#{dash}#{option}"
   ARGV << value.to_s unless value.nil?
-end
-
-# Get a RSA private key.
-# Always same key to speed up tests.
-def ca_root_key
-  @ca_root_key ||= OpenSSL::PKey::RSA.new(LetsCert::TEST::KEY_LENGTH)
-end
-
-# Generate a certificate for example.org, signed by #ca_root_key
-# @return [cert, domains]
-def generate_signed_cert
-  unless @generated_signed_cert
-    domains = %w(example.org www.example.org)
-
-    key = OpenSSL::PKey::RSA.new(LetsCert::TEST::KEY_LENGTH)
-    cert = OpenSSL::X509::Certificate.new
-    cert.version = 2
-    cert.serial = 2
-    cert.issuer = OpenSSL::X509::Name.parse "/DC=letscert/CN=CA"
-    cert.public_key = key.public_key
-    cert.not_before = Time.now
-    # 20 days validity
-    cert.not_after = cert.not_before + 20 * 24 * 60 * 60
-    ef = OpenSSL::X509::ExtensionFactory.new
-    ef.subject_certificate = cert
-    domains.each do |domain|
-      cert.add_extension(ef.create_extension('subjectAltName',
-                                              "DNS:#{domain}",
-                                              false))
-    end
-    cert.sign(ca_root_key, OpenSSL::Digest::SHA256.new)
-
-    @generated_signed_cert = [cert, domains]
-  end
-
-  @generated_signed_cert
 end
